a83edd3ddf80865444e9a7648a6852afa1b71713
aweisser <armin.weisser@gmail.com>
2020-08-06 22:45:55 +0200

    Remove obsolete ; at end of line

--- update_data.py
+++ update_data.py
@@ -2,7 +2,7 @@ import requests, json, sys
 outfile_name = sys.argv[1]
 url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.json"
 source = requests.get(url).json()
-countries = ["DEU", "FRA", "ITA", "USA", "IND", "GBR", "SWE"];
+countries = ["DEU", "FRA", "ITA", "USA", "IND", "GBR", "SWE"]
 result = []
 for country in countries:
     result.append(source[country])
