140c31b3024c7f0d19374d752a3e21f99f6d17ab
aweisser <armin.weisser@gmail.com>
2020-08-06 22:08:11 +0200

    update_data now filters and creates a .json file

--- update_data.py
+++ update_data.py
@@ -1,5 +1,10 @@
-import requests
-url = "https://github.com/owid/covid-19-data/blob/master/public/data/owid-covid-data.json"
-data = requests.get(url).json()
-print(data)
+import requests, json
+url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.json"
+source = requests.get(url).json()
+countries = ["DEU", "FRA", "ITA", "USA", "IND", "GBR", "SWE"];
+result = []
+for country in countries:
+    result.append(source[country])
 
+with open('data.json', 'w') as outfile:
+    json.dump(result, outfile)
