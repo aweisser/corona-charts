bacccc34cc71e2010dc4b5d2d4ff9ec143cf5b36
Armin Weißer <armin.weisser@gmail.com>
2020-08-06 20:36:36 +0200

    Update update_data.py

--- update_data.py
+++ update_data.py
@@ -1,1 +1,4 @@
-print("Hello")
+import urllib.request, json 
+with urllib.request.urlopen("http://maps.googleapis.com/maps/api/geocode/json?address=google") as url:
+    data = json.loads(url.read().decode())
+    print(data)
