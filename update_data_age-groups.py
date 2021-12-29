05b12631035173a4b39be25eb4c82d3475bf0c7d
aweisser <armin.weisser@gmail.com>
2021-12-29 20:15:11 +0100

    Germany: Add population data from Genesis

--- update_data_age-groups.py
+++ update_data_age-groups.py
@@ -1,20 +1,29 @@
-import requests, json
+import requests, json, re
 from datetime import datetime
 from datetime import timedelta
 
 start_date = "2020-01-01"
 dataPoints = []
 categories = []
+categoryKey = 'Altersgruppe'
+dateKey = 'Refdatum'
+categoryPattern = re.compile(r'^(A\d\d)(-|\+)(A\d\d)?$')
 
 # Get base data
 # Group by Refdatum,Altersgruppe as HTML: https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=&objectIds=&time=&resultType=none&outFields=Refdatum%2CAltersgruppe%2CAnzahlFall%2CAnzahlTodesfall&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Refdatum+desc&groupByFieldsForStatistics=Refdatum%2CAltersgruppe&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlFall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlFall%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlTodesfall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlTodesfall%22%0D%0A++%7D%0D%0A%5D&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=html&token=
 url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=&objectIds=&time=&resultType=none&outFields=Refdatum%2CAltersgruppe%2CAnzahlFall%2CAnzahlTodesfall&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Refdatum+desc&groupByFieldsForStatistics=Refdatum%2CAltersgruppe&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlFall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlFall%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlTodesfall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlTodesfall%22%0D%0A++%7D%0D%0A%5D&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token="
 dataPoints = list(requests.get(url).json()["features"])
+print(f"Got '{len(dataPoints)}' data points from RKI.")
 
-print(f"Got '{len(dataPoints)}' data points.")
-
-categoryKey = 'Altersgruppe'
-dateKey = 'Refdatum'
+# Get additional population by age data from Genesis (Statistisches Bundesamt)
+# Human readable version of this data table: https://www-genesis.destatis.de/genesis//online?operation=table&code=12411-0005&bypass=true&levelindex=0&levelid=1640791908447#abreadcrumb
+urlGenesis = "https://www-genesis.destatis.de/genesisWS/rest/2020/data/tablefile?username=DE8CNGS5G6&password=UrMjX%s4Tcwm78ADUdv%&language=de&name=12411-0005&timeslices=1&format=ffcsv"
+populationCsv = requests.get(urlGenesis).content
+# Ignore the first row (header) and grep the last column of each row. This is the population. The index of the resulting array is the age.
+populationList = [ int(row.split(b';').pop()) for row in populationCsv.splitlines()[1:] ]
+# The last row is the total population. Get the last row and remove it from the list.
+populationTotal = populationList.pop()
+print(f"Got '{len(populationList)+1}' data points from Genesis.")
 
 # Parse different catagories
 for dataPoint in dataPoints:
@@ -27,6 +36,10 @@ for dataPoint in dataPoints:
 categories = sorted(set(categories))
 
 for category in categories:
+    if not categoryPattern.match(category):
+        print(f"Skipping category '{category}'. It doesn't match the pattern '{categoryPattern.pattern}'.")
+        continue
+
     categoryDataPointsDict = {
         datetime.fromtimestamp(int(d['attributes']['Refdatum']/1000)).strftime("%Y-%m-%d")
         :
@@ -34,8 +47,17 @@ for category in categories:
     }
     print(f"Got '{len(categoryDataPointsDict)}' data points for category {category}.")
 
+    # sum up population for this age group
+    range = [ int(n) for n in category.replace('A', '').replace('+', '').split('-') ]
+    if len(range) == 1:
+        population = sum(populationList[range[0]:])
+    if len(range) == 2:
+        population = sum(populationList[range[0]:range[1]+1])
+
     categoryData = {
         "name": category,
+        "populationTotal": populationTotal,
+        "population": population,
         "data": []
     }
 
