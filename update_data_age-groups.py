3837c8761f3c36e550973daa9d4a13272b6ee9d4
aweisser <armin.weisser@gmail.com>
2022-01-01 12:44:58 +0100

    Germany: Add category 'A00+'

--- update_data_age-groups.py
+++ update_data_age-groups.py
@@ -2,18 +2,26 @@ import requests, json, re
 from datetime import datetime
 from datetime import timedelta
 
+
 start_date = "2020-03-01"
 dataPoints = []
 categories = []
 categoryKey = 'Altersgruppe'
+allAges = "A00+"
 dateKey = 'Refdatum'
 categoryPattern = re.compile(r'^(A\d\d)(-|\+)(A\d\d)?$')
 
-# Get base data
+# Get base data group by ages
 # Group by Refdatum,Altersgruppe as HTML: https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=&objectIds=&time=&resultType=none&outFields=Refdatum%2CAltersgruppe%2CAnzahlFall%2CAnzahlTodesfall&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Refdatum+desc&groupByFieldsForStatistics=Refdatum%2CAltersgruppe&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlFall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlFall%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlTodesfall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlTodesfall%22%0D%0A++%7D%0D%0A%5D&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=html&token=
-url = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=&objectIds=&time=&resultType=none&outFields=Refdatum%2CAltersgruppe%2CAnzahlFall%2CAnzahlTodesfall&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Refdatum+desc&groupByFieldsForStatistics=Refdatum%2CAltersgruppe&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlFall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlFall%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlTodesfall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlTodesfall%22%0D%0A++%7D%0D%0A%5D&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token="
-dataPoints = list(requests.get(url).json()["features"])
-print(f"Got '{len(dataPoints)}' data points from arcgis.")
+urlArcgis = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=&objectIds=&time=&resultType=none&outFields=Refdatum%2CAltersgruppe%2CAnzahlFall%2CAnzahlTodesfall&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Refdatum+desc&groupByFieldsForStatistics=Refdatum%2CAltersgruppe&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlFall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlFall%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlTodesfall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlTodesfall%22%0D%0A++%7D%0D%0A%5D&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token="
+dataPoints = list(requests.get(urlArcgis).json()["features"])
+print(f"Got '{len(dataPoints)}' data points from arcgis group by Refdatum and Altersgruppe.")
+
+# Get base data for all ages
+# Group by Refdatum as HTML: https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=&objectIds=&time=&resultType=none&outFields=Refdatum%2CAnzahlFall%2CAnzahlTodesfall&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Refdatum+desc&groupByFieldsForStatistics=Refdatum&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlFall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlFall%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlTodesfall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlTodesfall%22%0D%0A++%7D%0D%0A%5D&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=html&token=
+urlArcgisAll = "https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_COVID19/FeatureServer/0/query?where=&objectIds=&time=&resultType=none&outFields=Refdatum%2CAnzahlFall%2CAnzahlTodesfall&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=Refdatum+desc&groupByFieldsForStatistics=Refdatum&outStatistics=%5B%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlFall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlFall%22%0D%0A++%7D%2C%0D%0A++%7B%0D%0A++++%22statisticType%22%3A+%22sum%22%2C%0D%0A++++%22onStatisticField%22%3A+%22AnzahlTodesfall%22%2C%0D%0A++++%22outStatisticFieldName%22%3A+%22AnzahlTodesfall%22%0D%0A++%7D%0D%0A%5D&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token="
+dataPointsAll = list(requests.get(urlArcgisAll).json()["features"])
+print(f"Got '{len(dataPointsAll)}' data points from arcgis group by Refdatum.")
 
 # Get additional population by age data from Genesis (Statistisches Bundesamt)
 # Human readable version of this data table: https://www-genesis.destatis.de/genesis//online?operation=table&code=12411-0005&bypass=true&levelindex=0&levelid=1640791908447#abreadcrumb
@@ -33,7 +41,7 @@ hospList = list(filter(lambda row: b'Bundesgebiet' in row ,[row.split(b',') for 
 hospDict = { (row[0]+b" "+row[3]).decode("utf-8") : int(row[4]) for row in hospList }
 print(f"Got {len(hospDict)} data points from RKI Github.")
 
-# Parse different catagories
+# Parse other catagories
 for dataPoint in dataPoints:
     dataPoint = dataPoint['attributes']
     category = dataPoint[categoryKey]
@@ -42,13 +50,14 @@ for dataPoint in dataPoints:
         categories.append(category)
 
 categories = sorted(set(categories))
+categories.insert(0, allAges)
 
 for category in categories:
-
+    baseData = dataPointsAll if category == allAges else dataPoints
     categoryDataPointsDict = {
         datetime.fromtimestamp(int(d['attributes']['Refdatum']/1000)).strftime("%Y-%m-%d")
         :
-        d['attributes'] for d in dataPoints if d['attributes'][categoryKey] == category
+        d['attributes'] for d in baseData if category == allAges or d['attributes'][categoryKey] == category
     }
     print(f"Got '{len(categoryDataPointsDict)}' data points for category {category}.")
 
@@ -56,7 +65,7 @@ for category in categories:
     range = [ int(n) for n in category.replace('A', '').replace('+', '').split('-') ]
     if len(range) == 1:
         population = sum(populationList[range[0]:])
-    if len(range) == 2:
+    elif len(range) == 2:
         population = sum(populationList[range[0]:range[1]+1])
 
     categoryData = {
@@ -82,6 +91,9 @@ for category in categories:
         hospValue = hospDict.get(date+' '+category.replace('A', ''))
         if hospValue:
             day['AnzahlHospitalisierung7T'] = hospValue
+
+        # Make sure that category is set
+        day[categoryKey] = category
 
         # add day to country data
         categoryData["data"].append(day)
